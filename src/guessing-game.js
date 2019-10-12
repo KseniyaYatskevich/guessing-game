baa94bd0077ac1f5ac26be7b35810112939a9959
KseniaYatskevich <kseniayatskevich@gmail.com>
2019-10-13 00:28:02 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessMiddle = Math.ceil((this.max + this.min)/2);
+        return this.guessMiddle;
     }
 
     lower() {
-
+        this.min = this.min;
+        this.max = this.guessMiddle;
     }
 
     greater() {
-
+        this.min = this.guessMiddle;
+        this.max = this.max;
     }
 }
 
